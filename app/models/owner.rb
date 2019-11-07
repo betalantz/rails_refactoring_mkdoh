e58447fa979ad305b6e733a12da84310cb14d061
betalantz <betalantz@gmail.com>
2019-11-07 12:27:19 -0800

    adds .dog_owners scope method to Owner

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,4 +2,8 @@ class Owner < ApplicationRecord
   has_many :dogs
   has_many :shelters, through: :dogs
   validates :name, presence: true
+
+  def self.dog_owners
+    all.reject{|owner| owner.dogs.length == 0}
+  end
 end
